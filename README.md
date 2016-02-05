30f03f067d41aff3779870fad2c61a8af1e29b7a
Fang Wang <fangwang.me@gmail.com>
2016-02-05 17:10:19 +0800

    init & add crawl post link code

---
+++ README.md
@@ -0,0 +1,2 @@
+# drops_offline
+抓取 wooyun drops 文章，并且按照规则生成 PDF 文档
---
+++ crawl/crawl_post_list.py
@@ -0,0 +1,68 @@
+#coding=utf-8
+
+import requests
+from lxml import html
+import cPickle
+import time
+
+
+origin_url = 'http://drops.wooyun.org/'
+
+
+def crawl_all_post_links():
+
+    crawl_flag = True
+
+    page_num = 1
+    link_list = []
+    while crawl_flag:
+        crawl_url = origin_url + 'page/' + str(page_num)
+        print crawl_url
+
+        r = requests.get(crawl_url)
+        content = r.text.encode('utf-8')
+
+        if len(content) < 1000:
+            crawl_flag = False
+
+        each_page_links = parse_links(content)
+        if not each_page_links:
+            break
+        else:
+            link_list += each_page_links
+
+        page_num += 1
+        time.sleep(5)
+
+    with open('../data/links.pickle', 'w') as f:
+        cPickle.dump(f, link_list)
+
+    return
+
+
+def parse_links(content):
+    """
+    利用 lxml 解析文章链接,返回该页面中所有文章的链接
+    :parameter:content
+    :return:link list
+    """
+
+    each_page_link = []
+
+    try:
+        ele = html.fromstring(content)
+    except Exception, e:
+        print 'parse links failed, error: %s' % str(e)
+        return each_page_link
+
+    try:
+        link_ele_list = ele.find_class('entry-title')
+        each_page_link = [each_ele.xpath('a/@href') for each_ele in link_ele_list]
+    except Exception, e:
+        print 'parse links failed, error : %s' % str(e)
+
+    return each_page_link
+
+
+if __name__ == '__main__':
+    crawl_all_post_links()
